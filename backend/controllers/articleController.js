2fb4c72e5c0351000f614238103df33fae8edddd
jiangzy <jiangzy1998@gmail.com>
2023-09-20 23:07:27 +0800

    feat:增加获取文章详情接口

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,7 +1,13 @@
 const Article = require("../models/Article");
 const { StatusCodes } = require('http-status-codes');
-const CustomError = require('../errors');
-const path = require('path');
+
+
+// 新建一篇文章
+const createArticle = async (req, res) => {
+  // TODO 需要做校验，目前先进行接口测试
+  const article = await Article.create(req.body);
+  res.status(StatusCodes.CREATED).json({ data:article });
+}
 
 // 获取所有的文章
 // page 从 1 开始
@@ -14,7 +20,9 @@ const getAllArticles = async (req, res) => {
   if(!size){
     size = 10;
   }
-  const articles = await Article.find({}).sort({updatedAt:-1}).skip(size * ( page - 1 ) ).limit(size);
+  // 通过 .select() 方法来选择要返回的字段。用空格分隔字段名称。
+  const resultFieds = "title excerpt articleID updatedAt";
+  const articles = await Article.find({}).select(resultFieds).sort({updatedAt:-1}).skip(size * ( page - 1 ) ).limit(size);
   res.status(StatusCodes.OK).json(
     {
       data:articles,
@@ -23,13 +31,20 @@ const getAllArticles = async (req, res) => {
   )
 } 
 
-const createArticle = async (req, res) => {
-  // TODO 需要做校验，目前先进行接口测试
-  const article = await Article.create(req.body);
-  res.status(StatusCodes.CREATED).json({ data:article });
+// 根据获取文章详情
+const getArticleById = async (req, res) => {
+  let { id } = req.param;
+  const articleID = Number.parseInt(id);
+  if(isNaN(articleID)){
+    res.status(StatusCodes.BAD_REQUEST).json({data:"", msg:'参数格式错误！'})
+  }
+  const article = await Article.find({articleID:articleID});
+  res.status(StatusCodes.OK).json({data:article})
 }
+
 
 module.exports = {
   getAllArticles,
+  getArticleById,
   createArticle
 }
--- frontend/src/pages/blog/ArtDetail/index.tsx
+++ frontend/src/pages/blog/ArtDetail/index.tsx
@@ -1,6 +1,23 @@
-import { Link } from "react-router-dom";
+import { fetchAPI } from "@/utils/apis/fetch";
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ArtDetail:React.FC = () => {
+
+  const { id } = useParams();
+
+  const fetchArticle = async (id:string | undefined) => {
+    if(!id) return;
+    const { data } = await fetchAPI(`/article/${id}`, 'GET', {});
+    if(!!data){
+      console.log(data);
+    }
+  }
+
+  useEffect(()=>{
+    fetchArticle(id);
+  }, [id])
+
   return (
     <section>
       <Link to='/blog'>B</Link>
