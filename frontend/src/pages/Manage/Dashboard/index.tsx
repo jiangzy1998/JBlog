9859ec96dcdf90cc77d0f2f979994799db9e5f29
jiangzy <jiangzy1998@gmail.com>
2023-10-20 23:38:35 +0800

    feat:后台页面开发

--- frontend/src/pages/Manage/Dashboard/index.tsx
+++ frontend/src/pages/Manage/Dashboard/index.tsx
@@ -3,15 +3,18 @@ import "./index.less"
 const Dashboard:React.FC = () => {
   return (
     <div className="dashboard">
-      <div className="dashboard-header width-10 height-small"></div>
-      <div className="bg-card width-3">
-        <div className="card-content height-small"></div>
+      <div className="dashboard-header width-10 height-sm">
+        <h3>Welecome back</h3>
+        <span>October 26</span>
       </div>
       <div className="bg-card width-3">
-        <div className="card-content height-small"></div>
+        <div className="card-content height-sm"></div>
       </div>
       <div className="bg-card width-3">
-        <div className="card-content height-small"></div>
+        <div className="card-content height-sm"></div>
+      </div>
+      <div className="bg-card width-3">
+        <div className="card-content height-sm"></div>
       </div>
 
       <div className="bg-card width-6">
